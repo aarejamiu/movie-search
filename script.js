97ffc49e9c925d3dc8f8195901216e52aca71d95
jamiu <aarejamiu1@gmail.com>
2026-03-07 07:20:40 +0100

    details page added and complete css

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const API_KEY = '7f12673c';
 async function search() {
     let searchInput = document.getElementById('searchInput').value;
+    localStorage.setItem("lastSearch", searchInput);
     let url = `http://www.omdbapi.com/?s=${searchInput}&apikey=${API_KEY}`;
     document.getElementById('loading').innerHTML = 'Loading...';
     try {
@@ -19,10 +20,12 @@ async function search() {
         movies.forEach(movie => {
 
         output += `
-        <div>
+        <div class="movie">
+        <a href="details.html?id=${movie.imdbID}">
         <img src="${movie.Poster}" width="200">
         <h3>${movie.Title}</h3>
         <p>${movie.Year}</p>
+        </a>
         </div>
         `;
 
@@ -32,4 +35,64 @@ async function search() {
         catch (error) {
         console.error('Error:', error);
     }
+}
+/* -------- DETAILS PAGE -------- */
+
+async function getMovieDetails(){
+
+let params = new URLSearchParams(window.location.search);
+
+let movieId = params.get("id");
+
+if(!movieId) return;
+
+let url = `https://www.omdbapi.com/?i=${movieId}&apikey=${API_KEY}`;
+
+try{
+
+let response = await fetch(url);
+let movie = await response.json();
+
+if(movie.Response === "False"){
+document.getElementById("movieDetails").innerHTML = "Movie not found";
+return;
+}
+
+let output = `
+<img src="${movie.Poster}">
+<h2>${movie.Title}</h2>
+
+<p><strong>Year:</strong> ${movie.Year}</p>
+<p><strong>Genre:</strong> ${movie.Genre}</p>
+<p><strong>Actors:</strong> ${movie.Actors}</p>
+<p><strong>Rating:</strong> ${movie.imdbRating}/10</p>
+<p><strong>Plot:</strong> ${movie.Plot}</p>
+`;
+
+document.getElementById("movieDetails").innerHTML = output;
+
+}
+
+catch(error){
+console.error(error);
+}
+
+}
+
+
+/* -------- RUN FUNCTIONS -------- */
+
+if(document.getElementById("movieDetails")){
+getMovieDetails();
+}
+
+window.onload = function(){
+
+let lastSearch = localStorage.getItem("lastSearch");
+
+if(lastSearch){
+document.getElementById("searchInput").value = lastSearch;
+search();
+}
+
 }
